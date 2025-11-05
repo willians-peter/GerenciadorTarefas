552b94937ea4703bcfddcd0bedc2f9a784e6d730
willians-peter <petercipriano@hotmail.com>
2025-11-05 14:48:02 -0300

    arquivo inicial spring
    iniciado projeto com gerador do arquivo spring

---
+++ src/test/java/spring/java/gerenciador/Tarefas/GerenciadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.java.gerenciador.Tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
